e2f21f8f9db4f64825c604000cd8a961ee0cba87
Freeman Liu <llw599502537@gmail.com>
2026-08-14 10:00:13 +0800

    done

--- supabase/migrations/20260813173150_allow_group_player_rejoin.sql
+++
@@ -1,6 +0,0 @@
-alter table public.group_player
-  drop constraint group_player_group_id_player_id_key;
-
-create unique index group_player_group_id_player_id_unique
-  on public.group_player (group_id, player_id)
-  where deleted_at is null;
